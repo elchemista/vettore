fcf4784a092210d50becfbcb169f571cdfab9bd3
Yuriy Zhar <darkyfelis@gmail.com>
2025-03-09 02:25:38 +0100

    checksum

---
+++ checksum-Elixir.Vettore.exs
@@ -0,0 +1,12 @@
+%{
+  "libvettore-v0.1.7-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:09ed0e7d1de48bde9d324c2ccffea974f727d48030ca6345474846aac288f937",
+  "libvettore-v0.1.7-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:cb90eb8c63783bc2ec5e6d12af80ff7615fd7e6257bdad82949682a23b9306c9",
+  "libvettore-v0.1.7-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:0e0d62017971a9501ca8672939306ba9fed8a4ab007b9be86fdd4bb49ffde791",
+  "libvettore-v0.1.7-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:58432ee011bbad61829027e6f742c1b57a2bfce2d4170d9b7acf19e81699bd59",
+  "libvettore-v0.1.7-nif-2.15-riscv64gc-unknown-linux-gnu.so.tar.gz" => "sha256:814d21678def5e7b396bbfeaee2105f158267ec27f6d6e7de02c064053c98781",
+  "libvettore-v0.1.7-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:ffc6ae3cb3074b804893fa6870af28b077bba046e5cc07af4357a1653af75e71",
+  "libvettore-v0.1.7-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:5d031924e69c5e80f61e403e818430c97b0c70f913649863e778d0ffe62c264a",
+  "libvettore-v0.1.7-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:c7859db4effdf009bbdfece221e57cfdecc4d6d046f6babc25d4e7ed660c07b3",
+  "vettore-v0.1.7-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:47da441634b514a5b8cd8bd3c007e2b61bc6a4eb29bab9a9ec8a868139b850a6",
+  "vettore-v0.1.7-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:788719e1384f596f7887f373fa0ac25eea81bc59ffe140160bcc0cd521dc21bf",
+}
